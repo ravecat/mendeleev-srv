a6d4a0d01c2b51ff86e1bb5077f4d9c2b702a73a
Max Sharov <maxsharoff@gmail.com>
2019-01-09 23:26:17 +0300

    Update environment
    Add prod/dev environment, ES6 support, test, linting.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mendeleev API' });
+  res.send('Mendeleev express API');
 });
 
 module.exports = router;
