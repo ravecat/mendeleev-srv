410c525567f3b232645b2f21bf192f523e881a3a
Max Sharov <maxsharoff@gmail.com>
2019-02-13 09:55:24 +0300

    Update dev environment for create mocked db

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ require('dotenv').config({ path: `${path || 'dev'}` });
 export default {
 	port: process.env.PORT || 3001,
 	databaseName: process.env.DB_NAME,
-	databaseHost: process.env.DB_HOST,
+	databaseHost: process.env.DB_HOST || 'localhost',
 	databasePort: process.env.DB_PORT || 27017,
 	errorStatus: process.env.ERROR_STATUS || 500,
 	bodyLimit: process.env.BODY_LIMIT || 100,
