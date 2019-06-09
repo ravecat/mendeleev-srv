60c5bd4e653522b0f421b1965da79db79dcac55a
Max Sharov <maxsharoff@gmail.com>
2019-06-09 12:23:34 +0300

    Init CI/CD pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM node:10.13-alpine as builder
 WORKDIR /app
 COPY . .
-RUN yarn install && ./node_modules/.bin/babel src -s -D -d dist
+RUN yarn install && \
+    ./node_modules/.bin/babel src -s -D -d dist
 
 FROM node:10.13-alpine
 WORKDIR /app
 COPY package*.json ./
-RUN yarn install --production && yarn cache clean
+RUN yarn install --production && \
+    yarn cache clean
 COPY --from=builder /app/dist ./dist
 EXPOSE 3000
 CMD ["node", "dist"]
--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const dotenvFiles = [
 ];
 
 if (!dotenvFiles.some(fs.existsSync)) {
-	console.warn('\nAny environment variables files don\'t exist, app uses default configuration\n');
+	console.warn('\nAny environment variables files don\'t exist, app uses manually setted parameters or default configuration\n');
 }
 
 dotenvFiles.forEach(dotenvFile => {
@@ -23,7 +23,7 @@ dotenvFiles.forEach(dotenvFile => {
 
 export default {
 	mode: NODE_ENV,
-	port: process.env.PORT || 3001,
+	port: process.env.PORT || 3000,
 	databaseName: process.env.DB_NAME || 'mendeleev',
 	databaseHost: process.env.DB_HOST || 'localhost',
 	databasePort: process.env.DB_PORT || 27017,
